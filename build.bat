8c67340c312ca437f80eb207f2528d67a67ee7fc
Frederic Desgreniers <fred@frde.me>
2017-04-02 18:05:56 -0400

    Fixed build issue

--- build.bat
+++ build.bat
@@ -3,14 +3,14 @@
 mkdir build
 pushd build
 
-set CompilerFlags=-FC -Zi /W1
+set CompilerFlags=-FC -Zi /W0
 
 set Files=..\main.cpp ..\Cube.cpp ..\Renderable.cpp ..\Terrain.cpp ..\Skybox.cpp  
 
-set Libs=user32.lib gdi32.lib opengl32.lib ..\LIBS\glfw3.lib msvcrt.lib msvcmrt.lib LIBCMT.lib Shell32.lib ..\LIBS\glew32s.lib ..\LIBS\SOIL.lib vcruntimed.lib ucrtd.lib
+set Libs=user32.lib gdi32.lib opengl32.lib ..\LIBS\glfw3.lib msvcrtd.lib msvcmrtd.lib LIBCMT.lib Shell32.lib ..\LIBS\glew32s.lib ..\LIBS\SOIL.lib 
 
 
-cl %Files% %CompilerFlags% %libs% /EHsc /I..\INCLUDE  
+cl %Files% %CompilerFlags% %libs% /EHsc /I..\INCLUDE
 
 
 popd
