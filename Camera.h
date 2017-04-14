f9d72d0795684a69cc54d5d862ba2b96c3d5bb4f
TarikAzz <tarikazzouni@hotmail.com>
2017-04-14 00:22:42 -0400

    Fixed conflcit

--- Camera.h
+++ Camera.h
@@ -11,19 +11,19 @@
 
 #define PI 3.14159265358979323846
 
-enum CameraMovement 
+enum CameraMovement
 {
-    FORWARD,
-    BACKWARD,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN,
-    ROLL_LEFT,
-    ROLL_RIGHT,
-    SPRINT,
-    WALK,
-    CRAWL
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	ROLL_LEFT,
+	ROLL_RIGHT,
+	SPRINT,
+	WALK,
+	CRAWL
 };
 
 
@@ -33,235 +33,240 @@ const GLfloat ROLL_SPEED = 1.5f;
 const GLfloat ZOOM = 45.0f;
 
 
-class Camera 
+class Camera
 {
-    
-    
-    public:
-    
-    Camera(glm::vec4 position = glm::vec4(0.0f, 0.0f, 30.0f, 0.0f), glm::quat cameraQuat = glm::angleAxis(float(PI), glm::vec3(0.0f, 1.0f, 0.0f)))
-        : movementSpeed(SPEED), mouseSensitivity(SENSITIVITY), zoom(ZOOM), targetZoom(ZOOM)
-    {
-        
-        this->position = position;
-        this->cameraQuat = cameraQuat;
-        this->updateView();
-        
-    }
-    
-    
-    glm::vec3 getPosition()
-    {
-        return position;
-    }
-    
-    glm::mat4 getViewMatrix() 
-    {
-        return viewMatrix;
-        
-    }
-    
-    
-    void processKeyboard(CameraMovement command, GLfloat deltaTime) 
-    {
-        
-        GLfloat velocity = this->movementSpeed * deltaTime;
-        
-        if (FORWARD == command) 
-        {
-            this->position += this->front * velocity;
-        }
-        
-        if (BACKWARD == command) 
-        {
-            this->position -= this->front * velocity;
-        }
-        
-        if (LEFT == command) 
-        {
-            this->position += this->right * velocity;
-        }
-        
-        if (RIGHT == command) 
-        {
-            this->position -= this->right * velocity;
-        }
-        
-        if (UP == command) 
-        {
-            this->position -= this->up * velocity;
-        }
-        
-        if (DOWN == command) 
-        {
-            this->position += this->up * velocity;
-        }
-        
-        if (ROLL_LEFT == command) 
-        {
-            keyRoll = -ROLL_SPEED * deltaTime;
-            this->updateView();
-        }
-        
-        if (ROLL_RIGHT == command) 
-        {
-            keyRoll = +ROLL_SPEED * deltaTime;
-            this->updateView();
-        }
-        
-        
-        if (SPRINT == command) 
-        {
-            this->movementSpeed = SPEED * 2.5;
-        }
-        
-        
-        if (WALK == command) 
-        {
-            this->movementSpeed = SPEED;
-        }
-        
-        if (CRAWL == command) 
-        {
-            this->movementSpeed = SPEED / 2.5;
-        }
-        
-        updateView();
-    }
-    
-    
-    
-    void processMouseMovement(GLfloat xOffset, GLfloat yOffset) 
-    {
-        
-        keyYaw = xOffset * this->mouseSensitivity;
-        keyPitch = -yOffset * this->mouseSensitivity;
-        
-        this->updateView();
-        
-    }
-    
-    
-    
-    void processMouseScroll(GLfloat yOffset) 
-    {
-        if (yOffset > 0) 
-        {
-            targetZoom -= 3;
-        }
-        else if (yOffset < 0) 
-        {
-            targetZoom += 3;
-        }
-        
-        if (targetZoom <= 5.0f) {
-            targetZoom = 5.0f;
-        }
-        
-        if (targetZoom >= 45.0f) {
-            targetZoom = 45.0f;
-        }
-    }
-    
-    
-    
-    GLfloat getSmoothedZoom(float deltaTime) 
-    {
-        if (targetZoom > zoom) 
-        {
-            if (zoom >= 5.0f && zoom <= 45.0f) 
-            {
-                zoom += 45.0f * deltaTime;
-            }
-            
-            if (zoom > targetZoom) 				
-            {
-                zoom = targetZoom;
-            }
-            
-            if (zoom <= 5.0f) 
-            {
-                zoom = 5.0f;
-            }
-            
-            if (zoom >= 45.0f) 
-            {
-                zoom = 45.0f;
-            }
-            
-        }
-        
-        else if (targetZoom < zoom) 
-        {
-            if (zoom >= 5.0f && zoom <= 45.0f) 
-            {
-                zoom -= 45.0f * deltaTime;
-            }
-            
-            if (zoom <= 5.0f) 
-            {
-                zoom = 5.0f;
-            }
-            
-            if (zoom >= 45.0f) 
-            {
-                zoom = 45.0f;
-            }
-            
-        }
-        
-        this->mouseSensitivity = SENSITIVITY * (this->zoom / 45);
-        
-        return this->zoom;
-    }
-    
-    
-    private:
-    
-    glm::vec3 position;
-    glm::vec3 front;
-    glm::vec3 up;
-    glm::vec3 right;
-    
-    glm::quat cameraQuat;
-    glm::vec3 eyeVector;
-    
-    GLfloat keyPitch = 0.0f, keyYaw = 0.0f, keyRoll = 0.0f;
-    
-    glm::mat4 viewMatrix;
-    
-    GLfloat movementSpeed;
-    GLfloat mouseSensitivity;
-    
-    GLfloat zoom;
-    GLint targetZoom;
-    
-    void updateView() 
-    {
-        
-        // Temporary frame quaternion from pitch, yaw, roll
-        glm::quat key_quat = glm::quat(glm::vec3(keyPitch, keyYaw, keyRoll));
-        
-        // Reset frame keys;
-        keyPitch = 0.0f;
-        keyYaw = 0.0f;
-        keyRoll = 0.0f;
-        
-        cameraQuat = key_quat * cameraQuat;
-        cameraQuat = glm::normalize(cameraQuat);
-        
-        glm::mat4 rotation = glm::mat4_cast(cameraQuat);
-        
-        glm::mat4 translation = glm::mat4(1.0f);
-        translation = glm::translate(translation, position);
-        
-        viewMatrix = rotation * translation;
-        
-        glm::mat4 mat = viewMatrix;
-        
-        front = glm::vec3(mat[0][2], mat[1][2], mat[2][2]);
-        right = glm::vec3(mat[0][0], mat[1][0], mat[2][0]);
-        up = glm::normalize(glm::cross(front, right));
-        
-    }
+
+
+public:
+
+	Camera(glm::vec4 position = glm::vec4(0.0f, 0.0f, 30.0f, 0.0f), glm::quat cameraQuat = glm::angleAxis(float(PI), glm::vec3(0.0f, 1.0f, 0.0f)))
+		: movementSpeed(SPEED), mouseSensitivity(SENSITIVITY), zoom(ZOOM), targetZoom(ZOOM)
+	{
+
+		this->position = position;
+		this->cameraQuat = cameraQuat;
+		this->updateView();
+
+	}
+
+
+	glm::vec3 getPosition()
+	{
+		return position;
+	}
+
+	void setPosition(glm::vec3 position)
+	{
+		this->position = position;
+	}
+
+	glm::mat4 getViewMatrix()
+	{
+		return viewMatrix;
+
+	}
+
+
+	void processKeyboard(CameraMovement command, GLfloat deltaTime)
+	{
+
+		GLfloat velocity = this->movementSpeed * deltaTime;
+
+		if (FORWARD == command)
+		{
+			this->position += this->front * velocity;
+		}
+
+		if (BACKWARD == command)
+		{
+			this->position -= this->front * velocity;
+		}
+
+		if (LEFT == command)
+		{
+			this->position += this->right * velocity;
+		}
+
+		if (RIGHT == command)
+		{
+			this->position -= this->right * velocity;
+		}
+
+		if (UP == command)
+		{
+			this->position -= this->up * velocity;
+		}
+
+		if (DOWN == command)
+		{
+			this->position += this->up * velocity;
+		}
+
+		if (ROLL_LEFT == command)
+		{
+			keyRoll = -ROLL_SPEED * deltaTime;
+			this->updateView();
+		}
+
+		if (ROLL_RIGHT == command)
+		{
+			keyRoll = +ROLL_SPEED * deltaTime;
+			this->updateView();
+		}
+
+
+		if (SPRINT == command)
+		{
+			this->movementSpeed = SPEED * 2.5;
+		}
+
+
+		if (WALK == command)
+		{
+			this->movementSpeed = SPEED;
+		}
+
+		if (CRAWL == command)
+		{
+			this->movementSpeed = SPEED / 2.5;
+		}
+
+		updateView();
+	}
+
+
+
+	void processMouseMovement(GLfloat xOffset, GLfloat yOffset)
+	{
+
+		keyYaw = xOffset * this->mouseSensitivity;
+		keyPitch = -yOffset * this->mouseSensitivity;
+
+		this->updateView();
+
+	}
+
+
+
+	void processMouseScroll(GLfloat yOffset)
+	{
+		if (yOffset > 0)
+		{
+			targetZoom -= 3;
+		}
+		else if (yOffset < 0)
+		{
+			targetZoom += 3;
+		}
+
+		if (targetZoom <= 5.0f) {
+			targetZoom = 5.0f;
+		}
+
+		if (targetZoom >= 45.0f) {
+			targetZoom = 45.0f;
+		}
+	}
+
+
+
+	GLfloat getSmoothedZoom(float deltaTime)
+	{
+		if (targetZoom > zoom)
+		{
+			if (zoom >= 5.0f && zoom <= 45.0f)
+			{
+				zoom += 45.0f * deltaTime;
+			}
+
+			if (zoom > targetZoom)
+			{
+				zoom = targetZoom;
+			}
+
+			if (zoom <= 5.0f)
+			{
+				zoom = 5.0f;
+			}
+
+			if (zoom >= 45.0f)
+			{
+				zoom = 45.0f;
+			}
+
+		}
+
+		else if (targetZoom < zoom)
+		{
+			if (zoom >= 5.0f && zoom <= 45.0f)
+			{
+				zoom -= 45.0f * deltaTime;
+			}
+
+			if (zoom <= 5.0f)
+			{
+				zoom = 5.0f;
+			}
+
+			if (zoom >= 45.0f)
+			{
+				zoom = 45.0f;
+			}
+
+		}
+
+		this->mouseSensitivity = SENSITIVITY * (this->zoom / 45);
+
+		return this->zoom;
+	}
+
+
+private:
+
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 up;
+	glm::vec3 right;
+
+	glm::quat cameraQuat;
+	glm::vec3 eyeVector;
+
+	GLfloat keyPitch = 0.0f, keyYaw = 0.0f, keyRoll = 0.0f;
+
+	glm::mat4 viewMatrix;
+
+	GLfloat movementSpeed;
+	GLfloat mouseSensitivity;
+
+	GLfloat zoom;
+	GLint targetZoom;
+
+	void updateView()
+	{
+
+		// Temporary frame quaternion from pitch, yaw, roll
+		glm::quat key_quat = glm::quat(glm::vec3(keyPitch, keyYaw, keyRoll));
+
+		// Reset frame keys;
+		keyPitch = 0.0f;
+		keyYaw = 0.0f;
+		keyRoll = 0.0f;
+
+		cameraQuat = key_quat * cameraQuat;
+		cameraQuat = glm::normalize(cameraQuat);
+
+		glm::mat4 rotation = glm::mat4_cast(cameraQuat);
+
+		glm::mat4 translation = glm::mat4(1.0f);
+		translation = glm::translate(translation, position);
+
+		viewMatrix = rotation * translation;
+
+		glm::mat4 mat = viewMatrix;
+
+		front = glm::vec3(mat[0][2], mat[1][2], mat[2][2]);
+		right = glm::vec3(mat[0][0], mat[1][0], mat[2][0]);
+		up = glm::normalize(glm::cross(front, right));
+
+	}
 };
--- main.cpp
+++ main.cpp
@@ -42,7 +42,6 @@ Terrain* terrain;
 
 
 
-
 // Free function signatures
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void cursorPosCallback(GLFWwindow* window, double xPos, double yPos);
@@ -51,196 +50,192 @@ void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
 void windowResizeCallback(GLFWwindow* window, int width, int height);
 void doMovement();
 
-void timerStart();
-void timerEnd();
-
 
 
 
 // ________________________________ MAIN ________________________________
 int main()
 {
-    srand(time(NULL));
-    
-    
-    
-    
-    // ___________________________ SETTINGS ___________________________
-    
-    // Create GLFW window
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-    
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Aquinea", nullptr, nullptr);
-    
-    glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
-    
-    if (window == nullptr)
-    {
-        std::cout << "Failed to create GLFW window!" << std::endl;
-        glfwTerminate();
-        exit(1);
-    }
-    
-    glfwMakeContextCurrent(window);
-    
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    
-    
-    // Set callback functions
-    glfwSetKeyCallback(window, keyCallback);
-    glfwSetMouseButtonCallback(window, mouseButtonCallback);
-    glfwSetCursorPosCallback(window, cursorPosCallback);
-    glfwSetWindowSizeCallback(window, windowResizeCallback);
-    glfwSetScrollCallback(window, scrollCallback);
-    
-    
-    // Initialize GLEW and OpenGL settings
-    glewExperimental = GL_TRUE;
-    
-    if (glewInit() != GLEW_OK)
-    {
-        std::cout << "Failed to initialize GLEW" << std::endl;
-        exit(1);
-    }
-    
-    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    
-    glPointSize(3);
-    
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    
-    // ___________________________ END SETTINGS ___________________________
-    
-    
-    
-    
-    
-    // ____________________________ CREATING SCENE ____________________________
-    
-    
-    // Generate skybox
-    Timer::start("skybox");
-    skybox = new Skybox();
-    Timer::stop("Skybox");
-    
-    // Generate terrain
-    Timer::start("terrain");
-    terrain = new Terrain();
-    Timer::stop("Terrain");
-    
-    
-    
-    //generate some fish objects
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> u1(0, 1);
-    
-    float terrainSize = terrain->getSize() * (terrain->getPointsPerChunk()-1);
-    
-    
-    Timer::start("fish");
-    for (int i = 0; i < 300; ++i)
-    {
-        objects.push_back(new Fish(glm::vec3(u1(gen) * terrainSize, u1(gen) * 100.0f, u1(gen) * terrainSize)));
-    }
-    Timer::stop("Fish");
-    
-    Timer::start("seaweed");
-    for(int i = 0; i < (int)(0.01f*(terrainSize*terrainSize)); i++)
-    {
-        float x = u1(gen) * terrainSize;
-        
-        float z = u1(gen) * terrainSize;
-        
-        float y = terrain->getHeightAt(x,z);
-        
-        TerrainChunk* chunk = terrain->getChunkAtReal((int)x,(int)z);
-        if(chunk != nullptr)
-            chunk->addEntity(new Seaweed(glm::vec3(x, y+1, z)));
-    }
-    Timer::stop("seaweed");
+	srand(time(NULL));
 
-	
-    
-    Timer::start("rock");
-    for(int i = 0; i < (int)(0.01f*terrainSize*terrainSize); i++)
-    {
-        float x = u1(gen) * terrainSize;
-        
-        float z = u1(gen) * terrainSize;
-        
-        float y = terrain->getHeightAt(x,z);
-        
-        TerrainChunk* chunk = terrain->getChunkAtReal((int)x,(int)z);
-        if(chunk != nullptr)
-            chunk->addEntity(new Rock(glm::vec3(-x, -y, -z)));
-    }
-    Timer::stop("rock");
-    // ____________________________ END CREATING SCENE ____________________________
-    
-    
 
-    
-	
-    // ___________________________ GAME LOOP ___________________________
-    
-    while (!glfwWindowShouldClose(window)) {
-        
-        // Update frame deltaTime
-        GLfloat currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-        
-        // Process events
-        glfwPollEvents();
-        doMovement();
-        
-        // Clear frame buffer
-        glClearColor(0.01f, 0.01f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        // Apply camera tranformations
-        glm::mat4 view = camera->getViewMatrix();
-        glm::mat4 projection = glm::perspective(glm::radians(camera->getSmoothedZoom(deltaTime)), (GLfloat)SCREEN_WIDTH / SCREEN_HEIGHT, 0.1f, 1000.0f);
-        
-        //Render skybox
-        skybox->render(view, projection);
-        terrain->render(camera->getPosition(), view, projection);
-		
-		
-        // Render objects
-        for (auto obj : objects)
-        {
-            obj->animate(deltaTime);
-            obj->render(view, projection);
-        }
-        
-        
-        
-        glfwSwapBuffers(window);
-    }
-    
-    // ___________________________ END GAME LOOP ___________________________
-    
-    
-    
-    
-    glfwDestroyWindow(window);
-    //TODO: object buffer cleanup
-    //glDeleteVertexArrays(1, &VAO);
-    //glDeleteBuffers(1, &VBO);
-    glfwTerminate();
-    
-    
+
+
+	// ___________________________ SETTINGS ___________________________
+
+
+
+	// Create GLFW window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Aquinea", nullptr, nullptr);
+	glfwHideWindow(window);
+
+	glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
+
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		exit(1);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+
+	// Set callback functions
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+	glfwSetCursorPosCallback(window, cursorPosCallback);
+	glfwSetWindowSizeCallback(window, windowResizeCallback);
+	glfwSetScrollCallback(window, scrollCallback);
+
+
+	// Initialize GLEW and OpenGL settings
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		exit(1);
+	}
+
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glPointSize(3);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	// ___________________________ END SETTINGS ___________________________
+
+
+
+
+
+	// ____________________________ CREATING SCENE ____________________________
+
+
+	// Generate skybox
+	Timer::start("skybox");
+	skybox = new Skybox();
+	Timer::stop("Skybox");
+
+	// Generate terrain
+	Timer::start("terrain");
+	terrain = new Terrain();
+	Timer::stop("Terrain");
+
+
+
+	//generate some fish objects
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> u1(0, 1);
+
+	float terrainSize = terrain->getSize() * (terrain->getPointsPerChunk() - 1);
+
+
+
+	Timer::start("fish");
+	for (int i = 0; i < 300; ++i)
+	{
+		objects.push_back(new Fish(glm::vec3(u1(gen) * terrainSize, u1(gen) * 100.0f + 10.0f, u1(gen) * terrainSize)));
+	}
+	Timer::stop("Fish");
+
+	Timer::start("seaweed");
+	for (int i = 0; i < (int)(0.01f*(terrainSize*terrainSize)); i++)
+	{
+
+		float x = u1(gen) * terrainSize;
+		float z = u1(gen) * terrainSize;
+		float y = terrain->getHeightAt(x, z);
+
+		TerrainChunk* chunk = terrain->getChunkAtReal((int)x, (int)z);
+		if (chunk != nullptr)
+			chunk->addEntity(new Seaweed(glm::vec3(x, y + 1, z)));
+	}
+	Timer::stop("seaweed");
+
+	Timer::start("rock");
+	for (int i = 0; i < (int)(0.01f*terrainSize*terrainSize); i++)
+	{
+		float x = u1(gen) * terrainSize;
+
+		float z = u1(gen) * terrainSize;
+
+		float y = terrain->getHeightAt(x, z);
+
+		TerrainChunk* chunk = terrain->getChunkAtReal((int)x, (int)z);
+		if (chunk != nullptr)
+			chunk->addEntity(new Rock(glm::vec3(-x, -y, -z)));
+	}
+	Timer::stop("rock");
+	// ____________________________ END CREATING SCENE ____________________________
+
+
+	camera->setPosition(glm::vec3(-float(terrainSize / 2), -40.0f, -float(terrainSize / 2)));
+
+
+	// ___________________________ GAME LOOP ___________________________
+	glfwShowWindow(window);
+	while (!glfwWindowShouldClose(window)) {
+
+		// Update frame deltaTime
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// Process events
+		glfwPollEvents();
+		doMovement();
+
+		// Clear frame buffer
+		glClearColor(0.01f, 0.01f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Apply camera tranformations
+		glm::mat4 view = camera->getViewMatrix();
+		glm::mat4 projection = glm::perspective(glm::radians(camera->getSmoothedZoom(deltaTime)), (GLfloat)SCREEN_WIDTH / SCREEN_HEIGHT, 0.1f, 1000.0f);
+
+		//Render skybox
+		skybox->render(view, projection);
+		terrain->render(camera->getPosition(), view, projection);
+		// Render objects
+		for (auto obj : objects)
+		{
+			obj->animate(deltaTime);
+			obj->render(view, projection);
+		}
+
+
+
+		glfwSwapBuffers(window);
+	}
+
+	// ___________________________ END GAME LOOP ___________________________
+
+
+
+
+	glfwDestroyWindow(window);
+	//TODO: object buffer cleanup
+	//glDeleteVertexArrays(1, &VAO);
+	//glDeleteBuffers(1, &VBO);
+	glfwTerminate();
+
+
 }
 
 // ________________________________ END MAIN ________________________________
@@ -252,50 +247,50 @@ int main()
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-        
-    }
-    
-    
-    
-    if (key >= 0 && key < 1024)
-    {
-        if (action == GLFW_PRESS)
-        {
-            keys[key] = true;
-        }
-        else if (action == GLFW_RELEASE)
-        {
-            keys[key] = false;
-        }
-    }
-    
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	}
+
+
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			keys[key] = true;
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			keys[key] = false;
+		}
+	}
+
 }
 
 
 
 void cursorPosCallback(GLFWwindow* window, double xPos, double yPos)
 {
-    mX = xPos;
-    mY = yPos;
-    
-    if (firstMouse)
-    {
-        lastX = xPos;
-        lastY = yPos;
-        
-        firstMouse = false;
-    }
-    
-    GLfloat xOffset = xPos - lastX;
-    GLfloat yOffset = lastY - yPos;
-    
-    lastX = xPos;
-    lastY = yPos;
-    
-    camera->processMouseMovement(xOffset, yOffset);
+	mX = xPos;
+	mY = yPos;
+
+	if (firstMouse)
+	{
+		lastX = xPos;
+		lastY = yPos;
+
+		firstMouse = false;
+	}
+
+	GLfloat xOffset = xPos - lastX;
+	GLfloat yOffset = lastY - yPos;
+
+	lastX = xPos;
+	lastY = yPos;
+
+	camera->processMouseMovement(xOffset, yOffset);
 }
 
 
@@ -303,89 +298,89 @@ void cursorPosCallback(GLFWwindow* window, double xPos, double yPos)
 
 void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
 {
-    camera->processMouseScroll(yOffset);
+	camera->processMouseScroll(yOffset);
 }
 
 
 
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 {
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
-    {
-        // SHOOT HARPOON
-        // TAKE PHOTO
-    }
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		// SHOOT HARPOON
+		// TAKE PHOTO
+	}
 }
 
 
 
 void windowResizeCallback(GLFWwindow* window, int width, int height)
 {
-    
-    SCREEN_WIDTH = width;
-    SCREEN_HEIGHT = height;
-    
-    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    
+
+	SCREEN_WIDTH = width;
+	SCREEN_HEIGHT = height;
+
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
 }
 
 
 
 void doMovement()
 {
-    if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
-    {
-        camera->processKeyboard(FORWARD, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN])
-    {
-        camera->processKeyboard(BACKWARD, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT])
-    {
-        camera->processKeyboard(LEFT, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
-    {
-        camera->processKeyboard(RIGHT, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_SPACE])
-    {
-        camera->processKeyboard(UP, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_LEFT_CONTROL])
-    {
-        camera->processKeyboard(DOWN, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_Q])
-    {
-        camera->processKeyboard(ROLL_LEFT, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_E])
-    {
-        camera->processKeyboard(ROLL_RIGHT, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_LEFT_SHIFT])
-    {
-        camera->processKeyboard(SPRINT, deltaTime);
-    }
-    
-    if (keys[GLFW_KEY_LEFT_ALT])
-    {
-        camera->processKeyboard(CRAWL, deltaTime);
-    }
-    
-    if (!keys[GLFW_KEY_LEFT_ALT] && !keys[GLFW_KEY_LEFT_SHIFT])
-    {
-        camera->processKeyboard(WALK, deltaTime);
-    }
-    
+	if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
+	{
+		camera->processKeyboard(FORWARD, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN])
+	{
+		camera->processKeyboard(BACKWARD, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT])
+	{
+		camera->processKeyboard(LEFT, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
+	{
+		camera->processKeyboard(RIGHT, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_SPACE])
+	{
+		camera->processKeyboard(UP, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_LEFT_CONTROL])
+	{
+		camera->processKeyboard(DOWN, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_Q])
+	{
+		camera->processKeyboard(ROLL_LEFT, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_E])
+	{
+		camera->processKeyboard(ROLL_RIGHT, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_LEFT_SHIFT])
+	{
+		camera->processKeyboard(SPRINT, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_LEFT_ALT])
+	{
+		camera->processKeyboard(CRAWL, deltaTime);
+	}
+
+	if (!keys[GLFW_KEY_LEFT_ALT] && !keys[GLFW_KEY_LEFT_SHIFT])
+	{
+		camera->processKeyboard(WALK, deltaTime);
+	}
+
 }
