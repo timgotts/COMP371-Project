03a62170755e09c56ceacfd64c338e202fb70562
Frederic Desgreniers <fred@frde.me>
2017-04-13 21:30:46 -0400

    Added rocks

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ pushd build
 
 set CompilerFlags=-FC -Zi /W0
 
-set Files=..\main.cpp ..\Cube.cpp ..\Seaweed.cpp  ..\Fish.cpp ..\Renderable.cpp ..\Terrain.cpp ..\TerrainGenerator.cpp  ..\Skybox.cpp ..\Config.cpp 
+set Files=..\main.cpp ..\Cube.cpp ..\Seaweed.cpp  ..\Fish.cpp ..\Rock.cpp ..\Renderable.cpp ..\Terrain.cpp ..\TerrainGenerator.cpp  ..\Skybox.cpp ..\Config.cpp 
 
 set Libs=user32.lib gdi32.lib opengl32.lib ..\LIBS\glfw3.lib msvcrtd.lib msvcmrtd.lib LIBCMT.lib Shell32.lib ..\LIBS\glew32s.lib ..\LIBS\SOIL.lib 
 
