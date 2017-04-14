73cc4c59c81e53dfedf89af805a62276b1f2591a
Frederic Desgreniers <fred@frde.me>
2017-04-14 08:49:55 -0400

    Color is now back to normal

--- res/shaders/terrain.fs
+++ res/shaders/terrain.fs
@@ -12,13 +12,13 @@ out vec4 color;
 void main()
 
 {
-	color = vec4(-vNorm.r, -vNorm.g, -vNorm.b, 1.0f);
+	//color = vec4(-vNorm.r, -vNorm.g, -vNorm.b, 1.0f);
 
-	/**color = vec4(
+	color = vec4(
 		0.961f - max(0, ((-vPos.y/1.0f) * 0.0195f)), 
 		0.840f - max(0, ((-vPos.y/1.0f) * 0.02734f)),
 		0.6875f - max(0, ((-vPos.y/1.0f) * 0.0234f))+0.2f, 
 		1.0f
-		);**/
+		);
 }
 
