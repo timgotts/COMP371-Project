465938f5a97d54e97476c3576b3b96c25903271e
Frederic Desgreniers <fred@frde.me>
2017-04-16 11:58:55 -0400

    updated build for glowfish
    # Conflicts:
    #	LightSource.h
    #	main.cpp

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ pushd build
 
 set CompilerFlags=-FC -Zi /W0
 
-set Files=..\main.cpp ..\Cube.cpp ..\Seaweed.cpp  ..\Fish.cpp ..\Renderable.cpp ..\Terrain.cpp ..\TerrainGenerator.cpp  ..\Skybox.cpp ..\Config.cpp ..\Rock.cpp ..\Material.cpp ..\DirectionalLight.cpp ..\LightSource.cpp ..\PointLight.cpp ..\Spotlight.cpp ..\Light.cpp
+set Files=..\main.cpp ..\Cube.cpp ..\Seaweed.cpp  ..\Fish.cpp ..\Renderable.cpp ..\Terrain.cpp ..\TerrainGenerator.cpp  ..\Skybox.cpp ..\Config.cpp ..\Rock.cpp ..\Material.cpp ..\DirectionalLight.cpp ..\LightSource.cpp ..\PointLight.cpp ..\Spotlight.cpp ..\Light.cpp ..\GlowFish.cpp
 
 set Libs=user32.lib gdi32.lib opengl32.lib ..\LIBS\glfw3.lib msvcrtd.lib msvcmrtd.lib LIBCMT.lib Shell32.lib ..\LIBS\glew32s.lib ..\LIBS\SOIL.lib 
 
