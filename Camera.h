635855c0df9ff264ff648c4edc1aa852f3e4ea33
Mathieu Leblanc <0mat.leb2@gmail.com>
2017-04-17 22:02:13 -0400

    Camera speed change

--- Camera.h
+++ Camera.h
@@ -27,9 +27,9 @@ enum CameraMovement
 };
 
 
-const GLfloat SPEED = 5* 7.0f;
+const GLfloat SPEED = 14.0f;
 const GLfloat SENSITIVITY = 0.0025f;
-const GLfloat ROLL_SPEED = 1.0f;
+const GLfloat ROLL_SPEED = 1.6f;
 const GLfloat ZOOM = 45.0f;
 
 
